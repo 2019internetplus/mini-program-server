780603c7505ce20b0c6b733ec4a28137ff99aa12
magbone <2300427529@qq.com>
2019-05-27 21:49:29 +0800

    add emtion value and faceback apis

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.post("/user/v0.1/login", function(req, res){
                   return user.addNewUser(value);
             }).then(value => {
                   console.log(value);
-                  const session = user.addUserSeesion(value);
+                  const session = user.addUserSession(value);
                   const resData = {
                         code: 100,
                         message: "success",
@@ -136,6 +136,133 @@ app.post("/user/v0.1/login", function(req, res){
       }
       
 })
+
+// add input value
+
+//test https://api.xumengli.cn/em/v0.1/addInputValue?token=TOKEN&commit=COMMIT&openid=OPENID&input_value=INPUT_VALUE&message=MESSAGE
+app.put('/em/v0.1/addInputValue', function(req, res){
+      if(req.query.token === undefined || req.query.commit === undefined || req.query.openid === undefined || req.query.input_value === undefined){
+            res.end(err.err401());
+      }else{
+            user.tokenValid(req.query.openid, req.query.token).then((value)=>{
+                  if(value){
+                        return mysqlOp.addInputValue(req.query.commit, req.query.input_value, req.query.openid, req.query.message)
+                  }else{
+                        res.end(err.err405());
+                  }
+            }).then((value) => {
+                  if(value == null) return;
+                  if(value.state === "Ok"){
+                        const resData = {
+                              code: 100,
+                              message: 'success',
+                              data: []
+                        };
+                        res.end(JSON.stringify(resData));
+                  }
+            })
+      }
+});
+// add test value
+//test https://api.xumengli.cn/em/v0.1/addTestValue?token=TOKEN&commit=COMMIT&openid=OPENID&test_value=INPUT_VALUE&self_affirm=SELF_AFFIRM&anti_anxiety=ANTI_ANXIETY&anti_melancholy=ANTI_MELANCHAOLY
+app.put('/em/v0.1/addTestValue', function(req, res){
+      if(req.query.token === undefined || req.query.commit === undefined || req.query.openid === undefined || req.query.test_value === undefined ||
+            req.query.self_affirm == undefined || req.query.anti_anxiety == undefined || req.query.anti_melancholy == undefined){
+            res.end(err.err401());
+      }else{
+            user.tokenValid(req.query.openid, req.query.token).then((value)=>{
+                  if(value){
+                        return mysqlOp.addTestValue(req.query.commit, req.query.test_value, req.query.openid, req.query.self_affirm, req.query.anti_anxiety, req.query.anti_melancholy)
+                  }else{
+                        res.end(err.err405());
+                  }
+            }).then((value) => {
+                  if(value == null) return;
+                  if(value.state === "Ok"){
+                        const resData = {
+                              code: 100,
+                              message: 'success',
+                              data: []
+                        };
+                        res.end(JSON.stringify(resData));
+                  }
+            })
+      }
+});
+
+// add faceback
+// test https://api.xumengli.cn/faceback/v0.1/add?openid=o7K0Z48OkwZi0LxTInXWGwdWlizM&message=hi&token=1558939295
+app.put('/faceback/v0.1/add', function(req, res){
+      if(req.query.openid == undefined || req.query.token == undefined || req.query.message == undefined){
+            res.end(err.err401());
+      }else{
+            user.tokenValid(req.query.openid, req.query.token).then((value) => {
+                  if(value){
+                        return mysqlOp.addFaceback(req.query.openid, req.query.message);
+                  }else{
+                        res.end(err.err405());
+                  }
+            }).then((value)=>{
+                  if(!value) return;
+                  if(value.state === "Ok"){
+                        const resData = {
+                              code: 100,
+                              message: 'success',
+                              data: []
+                        };
+                        res.end(JSON.stringify(resData));
+                  }
+            })
+      }
+})
+
+// push report of weekly | monthly while launching app where times.
+// test https://api.xumengli.cn/reports/v0.1/push?openid=OPENID&token=TOKEN
+
+app.get('/reports/v0.1/push', function(req, res){
+
+});
+
+// get emation datas from start to count.
+// test https://api.xumengli.cn/reports/v0.1/get?openid=OPENID&token=TOKEN&start=START&count=COUNT
+app.get('/reports/v0.1/get', function(req, res){
+      if(req.query.openid == undefined || req.query.token == undefined || req.query.count == undefined){
+            res.end(err.err401());
+      }else{
+            user.tokenValid(req.query.openid, req.query.token).then(value=>{
+                  if(value){
+                        return mysqlOp.getEmotionValue(req.query.openid, req.query.start || 0, req.query.count);
+                  }else{
+                        res.end(err.err405());
+                  }
+            }).then(value => {
+                  if(value.state == "Ok"){
+                        const  resData = {
+                              code: 100,
+                              message: "success",
+                              data: value.info
+                        };
+                        res.end(JSON.stringify(resData));
+                  }
+            }).catch(e => {
+                  console.log(e);
+                  res.end(err.err402());
+            })
+      }
+});
+
+app.get('/reports/v0.1/weekly/get', function(req, res){
+
+});
+
+app.get('/reports/v0.1/monthly/get', function(req, res){
+
+});
+
+app.get('/reports/v0.1/all/get', function(req, res){
+
+});
 var server = app.listen(8081, "127.0.0.1", function(){
       console.log("Server run: http://%s:%s", server.address().address, server.address().port);
 });
+ 
